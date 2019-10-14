774aed261a875c5588b842b9d083669148e82efd
RajPShinde <rajprakashshinde07@gmail.com>
2019-10-14 03:04:36 -0400

    (Unique ID 1.6) Added Tests for the methods and classes of controller

--- test/test.cpp
+++ test/test.cpp
@@ -34,8 +34,74 @@ OTHER DEALINGS IN THE SOFTWARE.
  *  @section
  */
 
+#include <cstdlib>
+#include <memory>
+
 #include <gtest/gtest.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include "Navigation.hpp"
+#include "SteerAlgorithm.hpp"
+
+/**
+ *  @brief Test to check the set functions
+ *  for kp_,ki_ and kd_
+ */
+TEST(Navigation, testSetPid) {
+Navigation testn;
+EXPECT_TRUE(testn.setKp_(1));
+EXPECT_TRUE(testn.setKi_(1));
+EXPECT_TRUE(testn.setKd_(1));
 }
+
+/**
+ *  @brief Test to check that values of
+ *  kp_,ki_ and kd_ are below 1 or not
+ */
+TEST(Navigation, testGetPid) {
+Navigation testn;
+EXPECT_LT(testn.getKp_(), 1);
+EXPECT_LT(testn.getKi_(), 1);
+EXPECT_LT(testn.getKd_(), 1);
+}
+
+/**
+ *  @brief Test to check the output of PID
+ *  Controller in the first cycle is within the set
+ *  bound
+ */
+TEST(Navigation, testVelocity) {
+Navigation testn;
+ASSERT_NEAR(testn.calculate(10, 30, 0), 30, 10);
+}
+
+/**
+ *  @brief Tests to check that the set functions works 
+ *  and the value of corrRadius in below setlimit
+ */
+TEST(SteerAlgorithm, testCorrRadius) {
+SteerAlgorithm tests;
+EXPECT_TRUE(tests.setCorrRadius_(10));	
+EXPECT_LT(tests.getCorrRadius_(), 20);
+}
+
+/**
+ *  @brief Tests to check if the wheel angles get 
+ *  resetted and the Ackeermann kinematic model is
+ *  is properly implemented
+ */
+TEST(SteerAlgorithm, testwheel) {
+SteerAlgorithm tests;
+EXPECT_TRUE(tests.resetWheel());
+EXPECT_LT(tests.changeWheelAngles(10, 4, 8), 46);
+}
+
+/**
+ *  @brief Test to check the functions arclength 
+ *  and turnTime provide right length and time values
+ */
+TEST(SteerAlgorithm, testCalculations) {
+SteerAlgorithm tests;
+EXPECT_LT(tests.arcLength(360, 10), 70);
+EXPECT_GT(tests.turnTime(3, 10), 0);
+}
+
